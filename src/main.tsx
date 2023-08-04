026c37b4d2be9e885de13a08eb38a6b6b3115365
david <palacingimenezdavid.fpllefia@gmail.com>
2023-08-04 19:49:58 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,25 @@ import App from './App.tsx'
 import './index.css'
 import CreateTask from './pages/CreateTask/CreateTask.tsx'
 import TaskDetail from './pages/TaskDetail/TaskDetail.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/create",
+    element: <CreateTask />
+  },
+  {
+    path: "/task/:id",
+    element: <TaskDetail />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/create" element={<CreateTask />} />
-        <Route path="/task/:id" element={<TaskDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
